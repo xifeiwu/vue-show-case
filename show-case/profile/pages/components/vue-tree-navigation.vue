132dd307fabbf6668d7d2d0d4e8ed47b6aee5c14
wuxifei <wuxifei@finupgroup.com>
2018-09-18 11:23:41 +0800

    update demo for vue-tree-navigation

--- show-case/profile/pages/components/vue-tree-navigation.vue
+++ show-case/profile/pages/components/vue-tree-navigation.vue
@@ -8,28 +8,75 @@
   export default {
     data() {
       return {
-        items: [
-          { name: 'Introduction', route: 'introduction', children: [
-            { name: 'Features', element: 'features' },
-            { name: 'Requirements', element: 'requirements' },
-            { name: 'Demo', element: 'demo' },
-          ]},
-          { name: 'Installation', route: 'installation' },
-          { name: 'Usage', route: 'usage', children: [
-            { name: 'Props', element: 'props', children: [
-              { name: 'items', element: 'items' },
-              { name: 'defaultOpenLevel', element: 'default-open-level' },
-            ]},
-          ]},
-          { name: 'Examples', route: 'examples', children: [
-            { name: 'Elements only', route: 'elements' },
-            { name: 'Routes only', route: 'routes' },
-            { name: 'Combined', route: 'combined' },
-            { name: 'This website', route: 'this' },
-          ]},
-          { name: 'Styling', route: 'styling' },
-          { name: 'Visit GitHub', external: 'https://github.com/MisRob/vue-tree-navigation' },
+        items: [{
+          name: 'Introduction',
+          route: 'introduction',
+          children: [{
+            name: 'Features',
+            element: 'features'
+          },
+            {
+              name: 'Requirements',
+              element: 'requirements'
+            },
+            {
+              name: 'Demo',
+              element: 'demo'
+            }
+          ]
+        },
+          {
+            name: 'Installation',
+            route: 'installation'
+          },
+          {
+            name: 'Usage',
+            route: 'usage',
+            children: [{
+              name: 'Props',
+              element: 'props',
+              children: [{
+                name: 'items',
+                element: 'items'
+              },
+                {
+                  name: 'defaultOpenLevel',
+                  element: 'default-open-level'
+                }
+              ]
+            }]
+          },
+          {
+            name: 'Examples',
+            route: 'examples',
+            children: [{
+              name: 'Elements only',
+              route: 'elements'
+            },
+              {
+                name: 'Routes only',
+                route: 'routes'
+              },
+              {
+                name: 'Combined',
+                route: 'combined'
+              },
+              {
+                name: 'This website',
+                route: 'this'
+              }
+            ]
+          },
+          {
+            name: 'Styling',
+            route: 'styling'
+          },
+          {
+            name: 'Visit GitHub',
+            external: 'https://github.com/MisRob/vue-tree-navigation'
+          }
         ],
+
       };
     },
   };
